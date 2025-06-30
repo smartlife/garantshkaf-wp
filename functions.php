bbb7616826ff12d04627c3eab303c068a895f789
Roman Vasilyev <vasilyev-r@mail.ru>
2025-06-30 19:42:26 +0300

    Add metabox

--- functions.php
+++ functions.php
@@ -328,6 +328,53 @@
 	/*** END Меняем URL страницы вида page/?location=moskva на URL вида page/moskva ***/
 	
 	
+	/*** ADD METABOX ***/
+	// Подключаем функцию активации мета блока (my_extra_fields)
+	add_action('add_meta_boxes', 'my_extra_fields', 1);
+
+	function my_extra_fields() {
+		add_meta_box( 'extra_fields', 'Галерея наших работ', 'extra_fields_box_func', 'portfolio', 'side', 'high' );
+	}
+
+	/* Код блока галереи */
+	function extra_fields_box_func( $post ){
+		for ($i=1; $i<=9; $i++) { ?>
+			<label>URL&#160;изображения <?php echo $i; ?>:</label>
+			<input type="text" name="extra[img-<?php echo $i; ?>]" value="<?php echo get_post_meta($post->ID, '_img-'.$i, 1); ?>" style="width: 100%;">
+			<div style="clear: both;"></div>
+		<? } ?>
+			<input type="hidden" name="extra_fields_nonce" value="<?php echo wp_create_nonce(__FILE__); ?>" />
+		<?php
+	}
+	
+	// включаем обновление полей при сохранении
+	add_action( 'save_post', 'my_extra_fields_update', 0 );
+
+	## Сохраняем данные, при сохранении поста
+	function my_extra_fields_update( $post_id ){
+		// базовая проверка
+		if (
+			   empty( $_POST['extra'] )
+			|| ! wp_verify_nonce( $_POST['extra_fields_nonce'], __FILE__ )
+			|| wp_is_post_autosave( $post_id )
+			|| wp_is_post_revision( $post_id )
+		)
+			return false;
+
+		// Все ОК! Теперь, нужно сохранить/удалить данные
+		//$_POST['extra'] = array_map( 'sanitize_text_field', $_POST['extra'] ); // чистим все данные от пробелов по краям
+		foreach( $_POST['extra'] as $key => $value ){
+			if( empty($value) ){
+				delete_post_meta( $post_id, '_'.$key ); // удаляем поле если значение пустое
+				continue;
+			}
+			update_post_meta( $post_id, '_'.$key, $value ); // add_post_meta() работает автоматически
+		}
+		return $post_id;
+	}
+	/*** END ADD METABOX ***/
+	
+	
 	/*** ДОБАВЛЯЕМ ВОЗМОЖНОСТЬ В НАСТРОЙКАХ ТЕМЫ ДОБАВИТЬ КОД СЧЕТЧИКА ***/
 	function mytheme_customize_register( $wp_customize ) {
 		// Добавляем секцию
